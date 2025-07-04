5eff9af24956a6e48a8fa675f264082a7bd881fb
beacastroalves <beacastro1008@gmail.com>
2025-07-04 16:35:48 +0100

    Implementando restart

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 const itemRefs = document.querySelectorAll('.item');
 const containerRef = document.querySelector('.container');
-const winningMessagetRef = document.querySelector('.data-winning-message');
-const winningMessageTextRef = document.querySelector('.data-winning-message-text');
+const winningRef = document.querySelector('.data-winning');
+const winningTextRef = document.querySelector('.data-winning-text');
+const restartButtonRef = document.querySelector('.data-restart-button');
 
 let isCircleTurn;
 
@@ -17,23 +18,28 @@ const winningCombinations = [
 ]
 
 const startGame = () => {
+  isCircleTurn = false;
+
   for (const item of itemRefs) {
+    item.classList.remove('o');
+    item.classList.remove('x');
+    item.removeEventListener('click', handleClick);
     item.addEventListener('click', handleClick, { once: true });
   };
 
-  isCircleTurn = false;
 
-  containerRef.classList.add('x');
+  setContainerHoverClass();
+  winningRef.classList.remove('show-message');
 }
 
 const endGame = (isDraw) => {
   if (isDraw) {
-    winningMessageTextRef.innerText = 'Empate!';
+    winningTextRef.innerText = 'Empate!';
   } else {
-    winningMessageTextRef.innerText = isCircleTurn ? 'O venceu!' : 'X venceu!';
+    winningTextRef.innerText = isCircleTurn ? 'O venceu!' : 'X venceu!';
   }
 
-  winningMessagetRef.classList.add('show-message');
+  winningRef.classList.add('show-message');
 }
 
 const checkForWin = (currentPlayer) => {
@@ -48,9 +54,7 @@ const placeMark = (item, classToAdd) => {
   item.classList.add(classToAdd);
 }
 
-const swapTurns = () => {
-  isCircleTurn = !isCircleTurn;
-
+const setContainerHoverClass = () => {
   containerRef.classList.remove('o');
   containerRef.classList.remove('x');
 
@@ -59,6 +63,11 @@ const swapTurns = () => {
   } else {
     containerRef.classList.add('x');
   }
+}
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+  setContainerHoverClass();
 }
 
 const handleClick = (e) => {
@@ -81,3 +90,5 @@ const handleClick = (e) => {
 }
 
 startGame();
+
+restartButtonRef.addEventListener('click', startGame);
