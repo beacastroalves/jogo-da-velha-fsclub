7405cc06a65e2f7e5f07e67ad04ed8a44581e2eb
beacastroalves <beacastro1008@gmail.com>
2025-07-04 16:34:49 +0100

    Verificando por vitória

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,20 @@
 const itemRefs = document.querySelectorAll('.item');
 const containerRef = document.querySelector('.container');
+const winningMessagetRef = document.querySelector('.data-winning-message');
+const winningMessageTextRef = document.querySelector('.data-winning-message-text');
 
 let isCircleTurn;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
 const startGame = () => {
   for (const item of itemRefs) {
@@ -11,6 +24,24 @@ const startGame = () => {
   isCircleTurn = false;
 
   containerRef.classList.add('x');
+}
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextRef.innerText = 'Empate!';
+  } else {
+    winningMessageTextRef.innerText = isCircleTurn ? 'O venceu!' : 'X venceu!';
+  }
+
+  winningMessagetRef.classList.add('show-message');
+}
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some(combination => {
+    return combination.every(index => {
+      return itemRefs[index].classList.contains(currentPlayer);
+    });
+  });
 }
 
 const placeMark = (item, classToAdd) => {
@@ -39,6 +70,12 @@ const handleClick = (e) => {
 
 
   // Verificar a cada clique se houve vitória
+  const isWin = checkForWin(classToAdd);
+
+  if (isWin) {
+    endGame(false);
+  }
+
   // Verificar a cada clique se houve empate
   swapTurns();
 }
